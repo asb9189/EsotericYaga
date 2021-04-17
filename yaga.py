010982ffe08a485a0c6ca197d20fe46f1951bb0f
CoachTiger <tigersgottaeat@gmail.com>
2021-04-17 14:43:53 -0400

    Fixed typo's when moving token / mem pointer

--- yaga.py
+++ yaga.py
@@ -12,15 +12,10 @@ mem_decrement_value = 1
 
 def main():
 
-    if len(sys.argv) == 1:
-        run_repl()
-    elif len(sys.argv) == 2:
+    if len(sys.argv) == 2:
         run_file(sys.argv[1])
     else:
         print("python3 yaga.py [file]")
-
-def run_repl():
-    pass
 
 def run_file(file):
     is_valid = valid_file(file)
@@ -55,25 +50,29 @@ def run(tokens):
                 token_pointer = tokens[token_pointer].goTo
             continue
         elif tokens[token_pointer].tokenType == tokenType.left:
-            if token_pointer - 1 >= mem_min_left:
-                token_pointer -= 1
+            if mem_pointer - 1 >= mem_min_left:
+                mem_pointer -= 1
         elif tokens[token_pointer].tokenType == tokenType.right:
-            if token_pointer + 1 <= mem_max_right:
-                token_pointer += 1
+            if mem_pointer + 1 <= mem_max_right:
+                mem_pointer += 1
         elif tokens[token_pointer].tokenType == tokenType.set_increment:
             mem_increment_value = tokens[token_pointer].value
         elif tokens[token_pointer].tokenType == tokenType.set_decrement:
             mem_decrement_value = tokens[token_pointer].value
         elif tokens[token_pointer].tokenType == tokenType.print_chars:
+            arr = ""
             for val in memory:
-                print(val, end='')
+                arr += chr(val)
+            print(arr)
         elif tokens[token_pointer].tokenType == tokenType.print_vals:
+            arr = ""
             for val in memory:
-                print(chr(val), end='')
+                arr += "[" + str(val) + "]"
+            print(arr)
         elif tokens[token_pointer].tokenType == tokenType.print_current_char:
-            print(chr(tokens[token_pointer]))
+            print(chr(tokens[token_pointer]) + "\n")
         elif tokens[token_pointer].tokenType == tokenType.print_current_val:
-            print(tokens[token_pointer])
+            print(tokens[token_pointer] + "\n")
         elif tokens[token_pointer].tokenType == tokenType.accept_input:
             stdin = input("> ")
             if type(stdin) is not int:
